e9d5f53fbc9ff29dde8b685691a7726f1dd9d860
aslxlce <aslxlce@outlook.com>
2025-01-18 17:08:43 +0100

    yohoo

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    function updateTotalPrice() {
+        const totalPriceElement = document.querySelector(".total");
+        const unitPrices = document.querySelectorAll(".unit-price");
+        const quantities = document.querySelectorAll(".quantity");
+        let total = 0;
+
+        unitPrices.forEach((priceElement, index) => {
+            const unitPrice = parseFloat(priceElement.textContent.replace("$", ""));
+            const quantity = parseInt(quantities[index].textContent);
+            total += unitPrice * quantity;
+        });
+
+        totalPriceElement.textContent = `${total} $`;
+    }
+
+    document
+        .querySelectorAll(".fa-plus-circle, .fa-minus-circle, .fa-trash-alt")
+        .forEach((button) => {
+            button.addEventListener("click", (e) => {
+                const quantityElement = e.target.parentElement.querySelector(".quantity");
+                let quantity = parseInt(quantityElement.innerText);
+
+                if (e.target.classList.contains("fa-plus-circle")) {
+                    quantity += 1;
+                } else if (e.target.classList.contains("fa-minus-circle") && quantity > 0) {
+                    quantity -= 1;
+                }
+                if (e.target.classList.contains("fa-trash-alt")) {
+                    quantity = 0;
+                }
+                quantityElement.textContent = quantity;
+                updateTotalPrice();
+            });
+        });
+
+    document.querySelectorAll(".fa-heart").forEach((heartIcon) => {
+        heartIcon.addEventListener("click", (e) => {
+            e.target.classList.toggle("liked");
+            e.target.style.color = e.target.classList.contains("liked") ? "red" : "black";
+        });
+    });
+
+    updateTotalPrice();
+});
